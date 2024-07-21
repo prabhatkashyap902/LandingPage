1942a4f50ff4751e37b8e2897f6457e0b95c0272
Prabhat <apple@apples-MacBook-Pro.local>
2024-07-21 09:59:02 +0530

    new images

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import moviePlay from './assets/images/movie_play2.png'
-import adultPlay from './assets/images/adult_play.png'
+import moviePlay from './assets/images/movie_play1.png'
+import adultPlay from './assets/images/adult_play1.png'
 
 
 function App() {
