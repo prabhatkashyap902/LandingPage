6ef7b106a87ed209ff1ad94c4ed455a3001a5b54
Prabhat <apple@apples-MacBook-Pro.local>
2024-07-22 17:53:33 +0530

    changed fonts

--- src/App.jsx
+++ src/App.jsx
@@ -32,11 +32,11 @@ function App() {
         Click on the Vite and React logos to learn more
       </p> */}
       {/* <p style={{color: 'white', position:'absolute', top: '2%', left: '50%', transform: 'translate(-50%, -50%)', fontSize: '30px', backgroundColor: 'black', padding: '10px', borderRadius: '10px'}}>MovieClip.com</p> */}
-      <p>पूरी मूवी देखने के लिए निचे वीडियो पे क्लिक करे 👇🏻</p>
-      <p>(Puri Movie dekhne ke liye niche video pe click kare) 👇🏻</p>
+      <p style={{fontSize: '20px'}}>पूरी मूवी देखने के लिए निचे वीडियो पे क्लिक करे 👇🏻</p>
+      <p style={{fontSize: '20px'}}>(Puri Movie dekhne ke liye niche video pe click kare) 👇🏻</p>
       <div><a href='https://ooloptou.net/4/7711738'><img src={moviePlay} style={{ width: '300px'}}/></a></div>
-      <p >18+ ya P*rn क्लिप देखने के लिए निचे वीडियो पे क्लिक करे </p>
-      <p>(18+ ya P*rn dekhne ke liye niche video pe click kare 👇🏻) </p>
+      <p style={{fontSize: '20px'}}>18+ क्लिप देखने के लिए निचे वीडियो पे क्लिक करे </p>
+      <p style={{fontSize: '20px'}}>(18+ clips dekhne ke liye niche video pe click kare 👇🏻) </p>
       <div><a href='https://ooloptou.net/4/7711738'><img src={adultPlay} style={{ width: '300px'}}/></a></div>
       {/* <div style={{position: 'absolute', bottom: '0', left: '50%', transform: 'translate(-50%, -50%)', width: '100%'}}>MovieClip.com copyright 2024</div> */}
     </>
